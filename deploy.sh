845290fbe8f12d481061dcfedc11efbd245e1b24
seergidev <seergi_dev@protonmail.com>
2023-04-02 19:54:49 +0200

    Preparing sparse checkout phase 1

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,19 @@ echo "All renders done"
 
 git add -A
 echo "Modifications added."
-git commit -m $1
+git commit -m \"$1\"
 echo "Commit with message done"
 
 # Deploy at github
+sh render_github.sh
 mv .gitignore .gitignorebak
 mv .gitignore.github .gitignore
 git push github main
 echo "Deployed to github"
+rm -rdf ./quarto .docs
 
 # Deploy at gitlab
+sh render_gitlab.sh
 mv .gitignore .gitignore.github
 mv .gitignore.gitlab .gitignore
 git push gitlab main
