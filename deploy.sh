2bb0c14a646fd58f4189712510a13c4b9d374361
seergidev <seergi_dev@protonmail.com>
2023-04-02 16:16:55 +0200

    Add deploy.sh and clean branch

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+rm -rdf ./.quarto ./docs ./public ./tiny
+
+sh render_github.sh
+sh render_gitlab.sh
+sh render_tiny.sh
+
+git add -A
+git commit -m $1
+
+# Deploy at github
+mv .gitigonore .gitignorebak
+mv .gitignore.github .gitignore
+sh render_github.sh
+git push github main
+
+# Deploy at gitlab
+mv .gitignore .gitignore.github
+mv .gitignore.gitlab .gitignore
+sh render_gitlab.sh
+git push gitlab main
+
+# Deploy at tiny
+mv .gitignore .gitignore.gitlab
+mv .gitigonre.tiny .gitignore
+sh render_tiny.sh
+git push tiny main
+
+# Rename again gitignore
+mv .gitignore .gitignore.tiny
+mv .gitignorebak .gitignore
+
+
+
+
---
+++ render_github.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+quarto render --site-url=https://seergidev.github.io --output-dir=docs
+
